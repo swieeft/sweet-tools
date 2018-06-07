040a96a93d8fd18f6b8767ae3f4ad383190fe0d2
박길남 <gilnam_park@genieworks.net>
2018-06-07 13:12:07 +0900

    라이브러리 테스트

---
+++ sweet-tools/String.swift
@@ -0,0 +1,16 @@
+//
+//  String.swift
+//  sweet-tools
+//
+//  Created by 박길남 on 2018. 6. 7..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    var test: String {
+        return "test"
+    }
+}
